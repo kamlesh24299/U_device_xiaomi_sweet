609de460e624978a00f0e85793debd210bc7a2f9
kamlesh24299 <130337802+kamlesh24299@users.noreply.github.com>
2023-10-26 14:08:59 +0530

    Update vendorsetup.sh

--- vendorsetup.sh
+++ vendorsetup.sh
@@ -1,18 +1,15 @@
 #kernel
-git clone -b lineage-20 --depth=1 https://github.com/pulkit077/kernel_xiaomi_sweet.git kernel/xiaomi/sweet
+git clone --depth=1 -b perf https://github.com/itsshashanksp/kernel_xiaomi_sm6150.git kernel/xiaomi/sweet
 
 #common_vendor
-git clone https://github.com/omnitrix21/android_vendor_xiaomi_sm6150-common.git -b U vendor/xiaomi/sm6150-common
+git clone https://github.com/kamlesh24299/vendor_xiaomi_sm6150-common.git -b U vendor/xiaomi/sm6150-common
 
 #common_tree
-git clone https://github.com/omnitrix21/device_xiaomi_sm6150-common.git -b U device/xiaomi/sm6150-common
+git clone https://github.com/kamlesh24299/device_xiaomi_sm6150-common.git -b U device/xiaomi/sm6150-common
 
 #sweet_vendor
-git clone https://github.com/omnitrix21/android_vendor_xiaomi_sweet.git -b U vendor/xiaomi/sweet
-
-#remove
-rm -rf hardware/xiaomi
+git clone https://github.com/kamlesh24299/U_vendor_xiaomi_sweet.git -b U vendor/xiaomi/sweet
 
 #hardware_xiaomi
-git clone https://github.com/PixysOS-Devices/hardware_xiaomi  -b thirteen hardware/xiaomi
+git clone https://github.com/LineageOS/android_hardware_xiaomi.git -b lineage-21.0 hardware/xiaomi
 
